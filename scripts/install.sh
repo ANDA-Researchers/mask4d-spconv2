70bbda9cc59c45cf19b484ab603a51b275203254
vuong-viet-hung <vuongviethung156@gmail.com>
2026-05-23 14:57:58 +0900

    fix: fix installation script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,20 +1,41 @@
+set -e
+
+# Install curl
+if ! command -v curl &> /dev/null; then
+    sudo apt install -y curl
+fi
+
 # Install uv
-if !command -v uv &> /dev/null; then
-    curl -LsSf https://astral.sh/uv/install.sh | sh
-    export PATH=$HOME/.local/bin:$PATH
+if ! command -v uv &> /dev/null; then
+    curl -LSf https://astral.sh/uv/install.sh | sh
+    export PATH="$HOME/.local/bin:$PATH"
+fi
+
+if ! command -v nvcc &> /dev/null || ! nvcc --version | grep -q "release 11.3"; then
+    # Install conda
+    if ! command -v conda &> /dev/null; then
+        curl -fSLo \
+            "$HOME/Miniforge3.sh" \
+            "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
+        bash -b -p "$HOME/Miniforge3.sh"
+        source "$HOME/miniforge3/etc/profile.d/conda.sh"
+    fi
+    # Create conda environment with python and cudatoolkit-dev
+    conda create -y -n cudatoolkit-11-3 -c conda-forge python=3.8 cudatoolkit-dev=11.3
+    export CUDA_HOME="$HOME/miniforge3/envs/cuda-toolkit-11-3"
+else
+    export CUDA_HOME="$(dirname $(dirname $(which nvcc)))"
 fi
 
 # Create a virtual environment
 uv venv --clear
-
-# Install mask4d
-uv pip install -e .
 
 # Install pytorch
 uv pip install torch==1.12.0+cu113 torchvision==0.13.0+cu113 \
     --extra-index-url https://download.pytorch.org/whl/cu113
 
 # Install pytorch-geometric packages
+uv add "scipy>=1.10.1"
 uv pip install \
     torch-cluster==1.6.0+pt112cu113 \
     torch-scatter==2.1.0+pt112cu113 \
@@ -22,11 +43,11 @@ uv pip install \
     -f https://data.pyg.org/whl/torch-1.12.0+cu113.html --no-index
 uv add torch-geometric==1.7.2
 
-# Install sptr
+# Install the rest of the dependencies
+uv sync
+
+# Build sptr
 git submodule update --init --recursive
 cd thirdparty/SparseTransformer
-uv run setup.py install
+uv pip install . --no-build-isolation
 cd ../..
-
-# Install the rest of the dependencies
-uv sync --inexact
