573639498d90aae73f6135fcee6f07f286cb591c
vuong-viet-hung <vuongviethung156@gmail.com>
2026-05-23 12:16:52 +0900

    refactor: refactor `mask4d/convert_to_spconv2.py`

--- mask4d/convert_to_spconv2.py
+++ mask4d/convert_to_spconv2.py
@@ -1,8 +1,8 @@
-import argparse
 import torch
+from argparse import ArgumentParser, Namespace
 
 
-TARGET_KEYS = {
+TARGET_KEYS = [
     "backbone.unet.conv.2.weight",
     "backbone.unet.u.conv.2.weight",
     "backbone.unet.u.u.conv.2.weight",
@@ -11,15 +11,15 @@ TARGET_KEYS = {
     "backbone.unet.u.u.deconv.2.weight",
     "backbone.unet.u.deconv.2.weight",
     "backbone.unet.deconv.2.weight",
-}
+]
 
 
-def convert_checkpoint(in_path: str, out_path: str, target_keys=TARGET_KEYS):
+def main(args: Namespace) -> None:
 
-    checkpoint = torch.load(in_path, map_location="cpu")
+    checkpoint = torch.load(args.in_path)
     state_dict = checkpoint["state_dict"]
 
-    for key in target_keys:
+    for key in TARGET_KEYS:
 
         value = state_dict[key]
         print(f"Before: {value.shape}")
@@ -30,12 +30,12 @@ def convert_checkpoint(in_path: str, out_path: str, target_keys=TARGET_KEYS):
         state_dict[key] = value
 
     checkpoint["state_dict"] = state_dict
-    torch.save(checkpoint, out_path)
-    print(f"Saved converted checkpoint to {out_path}.")
+    torch.save(checkpoint, args.out_path)
+    print(f"Saved converted checkpoint to {args.out_path}.")
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("in_path")
-    parser.add_argument("out_path")
-    args = parser.parse_args()
-    convert_checkpoint(args.in_path, args.out_path)
+    parser = ArgumentParser()
+    parser.add_argument("in_path", type=str)
+    parser.add_argument("out_path", type=str)
+    main(parser.parse_args())
