0e714d5269ed001aa2f1157d5cea88910f5d2877
vuong-viet-hung <vuongviethung156@gmail.com>
2026-05-23 10:22:01 +0900

    chore: fix weight download and conversion script

--- scripts/download_weights.sh
+++ scripts/download_weights.sh
@@ -1,6 +1,6 @@
 # Download and convert Mask4D pretrained model
 wget -P weights https://www.ipb.uni-bonn.de/html/projects/mask_4d/mask4d.ckpt
-uv run python mask4d/convert_to_spconv2.py weights/mask4d.ckpt" "weights/mask4d_spconv2.ckpt
+uv run python mask4d/convert_to_spconv2.py weights/mask4d.ckpt weights/mask4d_spconv2.ckpt
 
 # Download and convert MaskPLS pretrained model
 wget -P weights https://www.ipb.uni-bonn.de/html/projects/mask_4d/maskpls.ckpt
